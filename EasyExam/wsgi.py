f0375b8d949e1c17a3b3870951dc485120c7d67c
Gokul Ganeshan I <igokulganesh@gmail.com>
2021-04-26 18:00:01 +0530

    Updated EasyExam

--- EasyExam/wsgi.py
+++ EasyExam/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DescriptiveAnswerEvalution project.
+WSGI config for EasyExam project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DescriptiveAnswerEvalution.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyExam.settings')
 
 application = get_wsgi_application()
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn EasyExam.wsgi --log-file
